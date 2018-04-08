a4019ef860ffb0b4ed64a94840bee0eb04d24ad4
Luke Wright <luke@simplyluke.com>
2018-04-08 14:57:24 -0500

    week 11

---
+++ _posts/2018-04-08-week-11.d
@@ -0,0 +1,34 @@
+
+---
+layout: post
+title:  "Week 11"
+date:   2018-04-08 14:07:53 -0600
+categories: update
+author: Luke Wright
+---
+## What did you do this past week?
+
+This week was pretty busy. I studied for and did pretty well on a chemistry Midterm Wednesday. My group and I had to scramble a bit but not too much to finish up our project ahead of the Thursday deadline and I was pleased with our end result. I unfortunately didn't get as much done as I'd hoped for my linear algebra class so I need to do a lot of catching up this week in that class ahead of our Midterm this week. I also got a good bit done at work. Our team had our parent's day Saturday. Unfortunately that winter weather came out of nowhere so it wasn't a very fun day to be on the water but we did the best we could.
+
+I finally feel like I'm back in the flow after spring break. I'm communicating with Spredfast where I'll be interning this summer and getting excited about returning for a second summer working with them.
+
+## What's in your way?
+
+Nothing big at the moment. I'm excited for the last few weeks of the semester and feel ready to finish strong.
+
+## What will you do next week?
+
+* Get ready for our tournament this weekend in Louisiana
+* Get started on the next project for this class
+* Study for and take my linear algebra Midterm
+* Work
+
+## What's my experience of the class?
+
+This week was frustrating for a few reasons. First, grades for the prior project weren't released until right before the next part was due. This made it difficult to adapt to feedback, and it also led to use losing some points because we deployed our next part a day or two ahead of the deadline but apparently the graders viewed that deployment in grading our prior project. Our group has also found some of the project requirements to be rather subjective and it's difficult not knowing exactly what's expected in order to get full points on a project. I'm personally also not a huge fan of the presentations before the projects are due as I haven't found them to be super productive.
+
+That said I liked implementing the searching, sorting, and filtering for this latest project. I think the projects do a good job of laying out a realistic and common set of features and best practices for modern software development.
+
+## What's my pick-of-the-week or tip-of-the-week?
+
+This week [Cloudflare released a public DNS service](https://blog.cloudflare.com/announcing-1111/) on the IP 1.1.1.1. It's pretty cool and privacy-focussed. It's similar to Google's DNS service, but good to see another major company providing one. I briefly tested and whether it or google was faster seemed to basically be a roll of the dice but both were much faster than my ISP's DNS. I also noticed 1.1.1.1 on a few sidewalks, which I guess may have been a marketing effort by cloudflare.
